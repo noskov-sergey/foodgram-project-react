bf73301ab0515b8fdc01d5cc3855b5eb6ac8bc8e
Noskov Sergey <noskov.sergey@mail.ru>
2022-08-13 13:54:54 +0500

    Вторая часть. Деплой 42

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -10,7 +10,7 @@ DEBUG = True
 
 #ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
-ALLOWED_HOSTS = ['62.84.122.169', '127.0.0.1', 'api', ]
+ALLOWED_HOSTS = ['62.84.122.169', '127.0.0.1', 'api', 'localhost', ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
