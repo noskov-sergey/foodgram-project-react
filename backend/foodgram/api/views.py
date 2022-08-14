54afee72b82247a7ad8ab86153482a7a632cb519
Noskov Sergey <noskov.sergey@mail.ru>
2022-08-14 11:14:06 +0500

    Вторая стадия. Ревью 2.3

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -88,24 +88,23 @@ class RecipeViewSet(viewsets.ModelViewSet):
             pk
         )
 
-    def canvas_method(self, dictionary):
+    def canvas_method(self, ingredients):
         """
         Метод сохранения списка покупок в формате PDF.
         """
         response = HttpResponse(content_type='application/pdf')
-        response[
-            'Content-Disposition'] = 'attachment; \
-        filename = "shopping_cart.pdf"'
+        response['Content-Disposition'] = (
+            'attachment;filename = "shopping_cart.pdf'
+        )
         begin_position_x, begin_position_y = 40, 650
         sheet = canvas.Canvas(response, pagesize=A4)
-        pdfmetrics.registerFont(TTFont('FreeSans',
-                                       'data/FreeSans.ttf'))
+        pdfmetrics.registerFont(TTFont('FreeSans','data/FreeSans.ttf'))
         sheet.setFont('FreeSans', 50)
         sheet.setTitle('Список покупок')
         sheet.drawString(begin_position_x,
                          begin_position_y + 40, 'Список покупок: ')
         sheet.setFont('FreeSans', 24)
-        for number, item in enumerate(dictionary, start=1):
+        for number, item in enumerate(ingredients, start=1):
             if begin_position_y < 100:
                 begin_position_y = 700
                 sheet.showPage()
